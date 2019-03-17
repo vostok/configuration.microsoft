38d58750e7a161e6bed023348d428d7d92761f68
Konstantin Chibasov <koheatel@gmail.com>
2019-03-17 20:39:33 +0700

    Add configuration builder extension, which can be used to configure hosting with `.AddVostok(source)`

---
+++ Vostok.Configuration.Microsoft/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Configuration;
+using IConfigurationSource = Vostok.Configuration.Abstractions.IConfigurationSource;
+
+namespace Vostok.Configuration.Microsoft
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddVostok(this IConfigurationBuilder configurationBuilder,
+                                                      IConfigurationSource vostokConfigurationSource)
+        {
+            return configurationBuilder.Add(new VostokConfigurationSource(vostokConfigurationSource));
+        }
+    }
+}
