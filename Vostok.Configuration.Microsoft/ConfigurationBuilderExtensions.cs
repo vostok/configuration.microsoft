8d5d6c44e69aba8c840d78fa6904f1024c6174b6
Konstantin Chibasov <koheatel@gmail.com>
2019-03-20 22:19:33 +0700

    Add docs & annotations for extension method

--- Vostok.Configuration.Microsoft/ConfigurationBuilderExtensions.cs
+++ Vostok.Configuration.Microsoft/ConfigurationBuilderExtensions.cs
@@ -1,13 +1,29 @@
+using System;
+using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 using IConfigurationSource = Vostok.Configuration.Abstractions.IConfigurationSource;
 
 namespace Vostok.Configuration.Microsoft
 {
+    /// <summary>
+    /// Extension methods for <see cref="IConfigurationBuilder"/>.
+    /// </summary>
+    [PublicAPI]
     public static class ConfigurationBuilderExtensions
     {
+        /// <summary>
+        ///     Adds vostok configuration source as provider for <see cref="IConfiguration"/>.
+        /// </summary>
         public static IConfigurationBuilder AddVostok(
-            this IConfigurationBuilder configurationBuilder,
-            IConfigurationSource vostokConfigurationSource) =>
-            configurationBuilder.Add(new VostokConfigurationSource(vostokConfigurationSource));
+            [NotNull] this IConfigurationBuilder configurationBuilder,
+            [NotNull] IConfigurationSource vostokConfigurationSource)
+        {
+            if (configurationBuilder == null)
+                throw new ArgumentNullException(nameof(configurationBuilder));
+            if (vostokConfigurationSource == null)
+                throw new ArgumentNullException(nameof(vostokConfigurationSource));
+
+            return configurationBuilder.Add(new VostokConfigurationSource(vostokConfigurationSource));
+        }
     }
 }
