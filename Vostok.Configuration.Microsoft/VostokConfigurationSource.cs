a94ba3f31f398d1c8457e7e714ef19f1e4d0d4b2
Konstantin Chibasov <koheatel@gmail.com>
2019-03-21 19:57:05 +0700

    Make configuration source & configuration provider public.

--- Vostok.Configuration.Microsoft/VostokConfigurationSource.cs
+++ Vostok.Configuration.Microsoft/VostokConfigurationSource.cs
@@ -1,16 +1,24 @@
+using System;
+using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 
 namespace Vostok.Configuration.Microsoft
 {
-    internal class VostokConfigurationSource : IConfigurationSource
+    /// <inheritdoc />
+    public class VostokConfigurationSource : IConfigurationSource
     {
         private readonly Abstractions.IConfigurationSource vostokConfigurationSource;
 
-        public VostokConfigurationSource(Abstractions.IConfigurationSource vostokConfigurationSource)
+        /// <summary>
+        ///     Creates new configuration source for <see cref="Configuration" /> using vostok <see cref="IConfigurationSource" />.
+        /// </summary>
+        public VostokConfigurationSource([NotNull] Abstractions.IConfigurationSource vostokConfigurationSource)
         {
-            this.vostokConfigurationSource = vostokConfigurationSource;
+            this.vostokConfigurationSource =
+                vostokConfigurationSource ?? throw new ArgumentNullException(nameof(vostokConfigurationSource));
         }
 
+        /// <inheritdoc />
         public IConfigurationProvider Build(IConfigurationBuilder builder) =>
             new VostokConfigurationProvider(vostokConfigurationSource);
     }
