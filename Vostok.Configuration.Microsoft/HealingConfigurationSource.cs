053833ea171b0542f0e65a2261c1a6d577b9303f
Konstantin Chibasov <koheatel@gmail.com>
2019-03-24 23:08:52 +0700

    Add healing observable & configuration source

---
+++ Vostok.Configuration.Microsoft/HealingConfigurationSource.cs
@@ -0,0 +1,19 @@
+using System;
+using Vostok.Configuration.Abstractions;
+using Vostok.Configuration.Abstractions.SettingsTree;
+
+namespace Vostok.Configuration.Microsoft
+{
+    internal class HealingConfigurationSource : IConfigurationSource
+    {
+        private readonly IConfigurationSource baseConfigurationSource;
+
+        public HealingConfigurationSource(IConfigurationSource baseConfigurationSource)
+        {
+            this.baseConfigurationSource = baseConfigurationSource;
+        }
+
+        public IObservable<(ISettingsNode settings, Exception error)> Observe() =>
+            new HealingObservable(baseConfigurationSource);
+    }
+}
